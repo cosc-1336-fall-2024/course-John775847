f3ffd2edbe487ac0cd7545ece41ca83e6a7c97b0
John775847 <john.swanstone@g.austincc.edu>
2024-09-05 17:10:40 -0600

    Homework 1

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,10 @@
-#
+import output
+
+def main(): 
+  value1 = int(input("enter input one"))
+  value2 = int(input("enter input two"))
+
+  result = output.multiply_numbers(value1, value1)
+  print("result: ", result)
+
+main()
